0bf1a79f7e43f807f562be58244ec9327568ed37
Emil Valeev <emil.musician@gmail.com>
2017-09-11 14:33:30 +0300

    Updated dotfiles
    Added perl-extensions for urxvt to postinstall script;
    Added perl-extensions to urxvt configuration;
    Added xrandr startup script.

--- postinstall.sh
+++ postinstall.sh
@@ -5,3 +5,6 @@ dependencies=(zsh rxvt-unicode git i3 nodejs)
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get -y install ${dependencies}
+
+wget -O ~/.urxvt/ext/keyboard-select https://raw.githubusercontent.com/muennich/urxvt-perls/master/keyboard-select
+wget -O ~/.urxvt/ext/font-size https://raw.githubusercontent.com/majutsushi/urxvt-font-size/master/font-size
