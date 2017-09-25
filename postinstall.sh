b600a966522523ed5960af0f2b8e56603c7c0f86
Emil Valeev <emil.musician@gmail.com>
2017-09-25 13:45:12 +0300

    Add Vundle to postinstall script

--- postinstall.sh
+++ postinstall.sh
@@ -8,3 +8,4 @@ sudo apt-get -y install ${dependencies}
 
 wget -O ~/.urxvt/ext/keyboard-select https://raw.githubusercontent.com/muennich/urxvt-perls/master/keyboard-select
 wget -O ~/.urxvt/ext/font-size https://raw.githubusercontent.com/majutsushi/urxvt-font-size/master/font-size
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
