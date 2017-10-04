d54377a5dff26e75322da33771bfd402d2fc8858
Emil Valeev <emil.musician@gmail.com>
2017-10-04 20:15:26 +0300

    Remove urxvt from postinstall and Rename it to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,6 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get -y -qq install \
 	zsh \
-	rxvt-unicode \
 	git \
 	i3 \
 	nodejs \
@@ -16,10 +15,6 @@ sudo apt-get -y -qq install \
 		w3m \
 		poppler-utils mediainfo
 
-# urxvt perl-extensions
-wget -O ~/.urxvt/ext/keyboard-select https://raw.githubusercontent.com/muennich/urxvt-perls/master/keyboard-select
-wget -O ~/.urxvt/ext/font-size https://raw.githubusercontent.com/majutsushi/urxvt-font-size/master/font-size
-
 # vim bundle manager
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
@@ -27,6 +22,3 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -s https://raw.githubusercontent.com/emil14/termite-install/master/termite-install.sh | bash
 cd ./termite-install/ && ./termite-install.sh
 rm -rf ./termite-install ./termite ./vte-ng
-
-# default config files to ~/.config/ranger
-ranger --copy-config=all
