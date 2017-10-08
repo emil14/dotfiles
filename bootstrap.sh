8e6c83781855fd111ae4735ae9ede135119a5c58
Emil Valeev <emil.musician@gmail.com>
2017-10-08 18:49:30 +0300

    Update bootstrap shell script
    remove unneeded 'termite' directory deletion;
    add vim plugin and lightline theme install commands.

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,10 +18,14 @@ sudo apt-get -y -qq install \
 # vim bundle manager
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# vim plugins and theme
+vim +PluginInstall +qall
+cp -r ~/.vim/bundle/onedark.vim/autoload/lightline/colorscheme/onedark.vim /.vim/autoload/lightline/colorscheme/
+
 # install termite via script
 curl -s https://raw.githubusercontent.com/emil14/termite-install/master/termite-install.sh | bash
 cd ./termite-install/ && ./termite-install.sh
-rm -rf ./termite-install ./termite ./vte-ng
+rm -rf ./termite-install ./vte-ng
 
 # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
