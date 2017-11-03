6cf0a8bbc4f969c4d55b7d1dffe65750a6cfeb72
Emil Valeev <emil.valeev@emlid.com>
2017-11-03 22:02:09 +0300

    Add nerd-fonts, ranger-devicons and some comments to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,18 +4,29 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y -qq install \
-  zsh \
-  git \
-  i3 \
-  python3-pip \
-  nodejs \
-  silversearcher-ag \
-  ranger \
+  zsh \ # cooler than your shell
+  git \ # VCS we deserved
+  i3 \ # improved tiling wm
+  silversearcher-ag \ # like grep or ack, but faster
+  python3-pip \ # tool for installing Python packages
+  nodejs \ # JavaScript runtime
+  ranger \ # VIM-inspired terminal filemanager
+    # and it's dependencies
     caca-utils \
     highlight \
     atool \
     w3m \
     poppler-utils mediainfo
+
+# nerd-fonts
+git clone https://github.com/ryanoasis/nerd-fonts
+cd nerd-fonts && ./install.sh
+cd ../ && rm -rf nerd-fonts
+
+# ranger-devicons
+git clone https://github.com/alexanderjeurissen/ranger_devicons
+cd ranger_devicons && make install
+cd ../ && rm -rf ranger_devicons
 
 # neovim
 curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
@@ -30,7 +41,7 @@ pip3 install --upgrade neovim
 vim +PluginInstall +qall
 cp -r ~/.vim/bundle/onedark.vim/autoload/lightline/colorscheme/onedark.vim /.vim/autoload/lightline/colorscheme/
 
-# install termite via script
+# termite
 curl -s https://raw.githubusercontent.com/emil14/termite-install/master/termite-install.sh | bash
 cd ./termite-install/ && ./termite-install.sh
 rm -rf ./termite-install ./vte-ng
@@ -38,5 +49,5 @@ rm -rf ./termite-install ./vte-ng
 # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# nvm
+# NVM
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
