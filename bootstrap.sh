8edf8ecbd7a82950cfbaae618ddfefebbdcbcc39
emil14 <emil.musician@gmail.com>
2018-06-15 19:03:24 +0300

    Remove Rust

--- bootstrap.sh
+++ bootstrap.sh
@@ -47,6 +47,3 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update
 sudo apt install code
-
-# Rust
-curl https://sh.rustup.rs -sSf | sh
