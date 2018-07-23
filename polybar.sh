e4f7cde92c90ba85e003421455066a8cd98249cf
emil14 <emil.musician@gmail.com>
2018-07-23 14:59:11 +0300

    Fix polybar multimonitor setup

--- polybar.sh
+++ polybar.sh
@@ -6,7 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar
-polybar example
+# Launch bar on every monitor
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload top &
+done
 
 echo "Bars launched..."
